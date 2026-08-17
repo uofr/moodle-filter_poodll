46386e88465034f92adc90363144f94d6e2fe4f4
John Lane <46030891+JohnOLane@users.noreply.github.com>
2026-08-17 13:35:02 -0600

    Adding more console log to see if uploader is on an assignment page.

--- amd/src/uploader.js
+++ amd/src/uploader.js
@@ -16,6 +16,8 @@ define(['jquery', 'core/log', 'filter_poodll/upskin_plain'], function ($, log, u
 
         init: function (element, config, upskin) {
             console.log('hello from uploader.js');
+            console.log(config);
+            console.log($('[name="action"]'));
             this.config = config;
             if (upskin) {
                 this.upskin = upskin;
