a85d7fe32a99096740b9edbeb1528914b39141b4
John Lane <46030891+JohnOLane@users.noreply.github.com>
2026-08-17 15:11:31 -0600

    Fix typo.

--- classes/task/adhoc_s3_move.php
+++ classes/task/adhoc_s3_move.php
@@ -59,7 +59,7 @@ class adhoc_s3_move extends \core\task\adhoc_task {
             mtrace('found context');
             mtrace($context->instanceid);
             mtrace($context->contextlevel);
-            if ($context->contextlevel == CONTEXT_COURSE) {
+            if ($context->contextlevel == CONTEXT_MODULE) {
                 $module = get_module_from_cmid($context->instanceid)[0];
                 mtrace('got module');
             }
