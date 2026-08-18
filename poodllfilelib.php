8bf5d3ff6739aa4831e86c308bc08d269df1683c
John Lane <46030891+JohnOLane@users.noreply.github.com>
2026-08-18 14:32:05 -0600

    Fixing upload type to be text instead of int.

--- poodllfilelib.php
+++ poodllfilelib.php
@@ -64,7 +64,7 @@ $mediatype = optional_param('mediatype', "", PARAM_TEXT);
 $filename = optional_param('filename', "", PARAM_TEXT);
 
 // extra params to handle assignments/quizzes
-$uploadtype = optional_param('uploadtype', '', PARAM_INT);
+$uploadtype = optional_param('uploadtype', '', PARAM_TEXT);
 
 //error log flags
 CONST LOG_PFL_TEMPDIR_FAIL = 1;
