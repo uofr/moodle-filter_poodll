b2db778e9e419d100c6a81628bb74b9c4f45a23d
John Lane <46030891+JohnOLane@users.noreply.github.com>
2026-09-08 15:43:52 -0600

    cleanup_failed_s3_move task execute function.

--- classes/task/cleanup_failed_s3_move.php
+++ classes/task/cleanup_failed_s3_move.php
@@ -16,6 +16,8 @@
 
 namespace filter_poodll\task;
 
+use moodle_exception;
+
 defined('MOODLE_INTERNAL') || die();
 
 /**
@@ -31,19 +33,78 @@ class cleanup_failed_s3_move extends \core\task\scheduled_task {
     }
 
     public function execute() {
-        global $DB;
+        global $CFG;
 
         mtrace('Execute cleanup_failed_s3_move task.');
 
-        $basefilename = 'poodllfile';
         $contenthash = \filter_poodll\poodlltools::fetch_placeholder_hash('audio');
+
+        try {
+            $placeholderfiles = $this->get_placeholder_files($contenthash);
+        }
+        catch (moodle_exception $exception) {
+            $errormessage = $exception->getMessage();
+            mtrace("ERROR: Could not get placeholder files: $errormessage.");
+            return;
+        }
+
+        if (count($placeholderfiles) == 0) {
+            mtrace('No placeholder files to be replaced.');
+            return;
+        }
+
+        foreach ($placeholderfiles as $placeholder) {
+            try {
+                $converteddrafts = $this->get_converted_draft_files($placeholder->filename, $contenthash);
+            }
+            catch (moodle_exception $exception) {
+                $errormessage = $exception->getMessage();
+                mtrace("ERROR: Could not get converted draft files for {$placeholder->filename}: $errormessage.");
+                continue;
+            }
+
+            $totalconverteddrafts = count($converteddrafts);
+            if ($totalconverteddrafts == 0) {
+                mtrace("Could not find converted draft files for {$placeholder->filename}.");
+                continue;
+            }
+            else if ($totalconverteddrafts > 1) {
+                mtrace("ERROR: Multiple converted draft files found for {$placeholder->filename}.");
+                continue;
+            }
+
+            $converteddraft = $converteddrafts[0];
+            $tempfilepath = $CFG->tempdir . "/" . $placeholder->filename;
+
+            try {
+                \filter_poodll\poodlltools::replace_placeholderfile_in_moodle($converteddraft, $placeholder, $tempfilepath);
+                mtrace("Updated $placeholder->filename:
+                 component: $placeholder->component
+                 filearea: $placeholder->filearea
+                 itemid: $placeholder->itemid");
+            }
+            catch (moodle_exception $exception) {
+                $errormessage = $exception->getMessage();
+                mtrace("ERROR: failed to replace $placeholder->filename: $errormessage");
+            }
+        }
+    }
+
+    /**
+     * Get files using the placeholder content hash that are not user draft files.
+     * @return array
+     */
+    private function get_placeholder_files($contenthash) {
+        global $DB;
+
+        $basefilename = 'poodllfile';
         $component = 'user';
 
         $like = $DB->sql_like('f.filename', ':basefilename');
         $query = "SELECT * FROM {files} f
-        WHERE $like
-        AND f.contenthash = :contenthash
-        AND f.component != :component";
+            WHERE $like
+            AND f.contenthash = :contenthash
+            AND f.component != :component";
 
         $params = [
            'basefilename' => "%$basefilename%",
@@ -51,8 +112,35 @@ class cleanup_failed_s3_move extends \core\task\scheduled_task {
            'component' => $component
         ];
 
-        $placeholderfiles = $DB->get_records_sql($query, $params);
-        mtrace(var_dump($placeholderfiles));
+        return $DB->get_records_sql($query, $params);
+    }
+
+    /**
+     * Get draft files for a given poodll file that are not using the placeholder content hash.
+     * @param string $filename - Name of poodll file.
+     * 
+     * @return array
+     */
+    private function get_converted_draft_files($filename, $contenthash) {
+        global $DB;
+
+        $component = 'user';
+        $filearea = 'draft';
+
+        $query = "SELECT * FROM {files} f
+            WHERE f.filename = :filename
+            AND f.contenthash != :contenthash
+            AND f.component = :component
+            AND f.filearea = :filearea";
+
+        $params = [
+           'filename' => $filename,
+           'contenthash' => $contenthash,
+           'component' => $component,
+           'filearea' => $filearea
+        ];
+
+        return $DB->get_records_sql($query, $params);
     }
 
 }
